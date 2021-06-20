47aa1a775152f32464d64ddf5225400dc93fc530
Derek Lorimer <dereklorime@bigpond.com>
2021-06-20 15:45:43 +1000

    DL - Initial commit

---
+++ CallSayHello/src/main/java/SayHello.java
@@ -0,0 +1,16 @@
+import com.dereklorimer.example.Hello;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hello hello = new Hello();
+		
+		hello.HelloWorld();
+		
+		
+
+	}
+
+}
