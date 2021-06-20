641cc74c6a703f09a0360fcdd96bc5614442c50a
Derek Lorimer <dereklorime@bigpond.com>
2021-06-20 15:23:28 +1000

    DL - Initial commit

---
+++ Core/Lib/src/main/java/com/dereklorimer/example/Hello.java
@@ -0,0 +1,24 @@
+package com.dereklorimer.example;
+
+import org.apache.log4j.Logger;
+
+public class Hello {
+	
+	final static Logger logger = Logger.getLogger(Hello.class);
+	
+	private static int VERSION = 3;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		logger.debug("Hello World!");
+
+	}
+
+	public void HelloWorld() {
+		
+		logger.debug("Version " + VERSION);
+
+	}
+
+}
