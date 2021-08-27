c544c318c08eaa369b85f8ada790205c992285b1
Derek Lorimer <dereklorime@bigpond.com>
2021-08-27 17:37:57 +1000

    DL - Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Returning text!";
+	}
+	
+	@GetMapping("/test")
+	public String index2() {
+		return "Returning text from test!";
+	}
+
+}
