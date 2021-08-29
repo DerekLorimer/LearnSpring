a464dbaf43a73bff1501a7a663b4556b1828b627
Derek Lorimer <dereklorime@bigpond.com>
2021-08-29 14:41:13 +1000

    DL - MockMvc example

---
+++ src/test/java/com/example/demo/DemoControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DemoControllerTest {
+	
+	@Autowired 
+	MockMvc mockMvc;
+
+	@Test
+	void test() throws Exception {
+				
+		mockMvc.perform(get("/"));
+	}
+
+}
